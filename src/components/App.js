b9036eb1a2460aba77300024310c68c23e141333
Paul HoangLong <paulhoanglong@gmail.com>
2024-01-18 16:26:23 -0600

    MVP demo with working shazam api

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,96 @@
-import { StatusBar } from "expo-status-bar";
-import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import { Text, View, TextInput, Button, Image, Linking } from "react-native";
 import styles from "./styles.jsx";
+import { useState } from "react";
+import axios from "axios";
+import { API_KEY } from "@env";
 
 export default function App() {
-  function onPress(){
-    console.log("Pressed");
-  }
+  const [search, setSearch] = useState("");
+  const [song, setSong] = useState({});
+
+  const handleSearch = () => {
+    console.log(search);
+    getSong(search);
+  };
+
+  // const handleChange = (event) => {
+  //   let search=this.state.search;
+  //   setSearch(search)
+  // }
+
+  //GET request
+  const getSong = (search) => {
+    axios({
+      method: "GET",
+      url: "https://shazam.p.rapidapi.com/search",
+      params: {
+        term: `${search}`,
+        locale: "en-US",
+        offset: "0",
+        limit: "5",
+      },
+      headers: {
+        "X-RapidAPI-Key": `${API_KEY}`,
+        "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+      },
+    })
+      .then((response) => {
+        console.log(
+          "Get Success:",
+          response.data.tracks.hits[0].track.images.coverart
+        );
+        setSong(response.data.tracks.hits[0].track);
+        console.log(song);
+      })
+      .catch((error) => {
+        console.log("Error Getting", error);
+      });
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! WOWZA</Text>
-      <Text>New text field Open up App.js to start working on your app! WOWZA </Text>
-      <Text>LMAOOpen up App.js to start working on your app! WOWZA</Text>
-      <StatusBar style="auto" />
-      <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Click Me</Text>
+      <Text>Enter a song name!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={(text) => setSearch(text)}
+          value={search}
+          placeholder="Type here to search"
+          style={styles.input}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button title="Search" color={"black"} onPress={handleSearch} />
+      </View>
+      {Object.keys(song).length ? (
+        <View style={styles.songContainer}>
+          <Image
+            style={styles.coverart}
+            source={{ uri: `${song.images.coverart}` }}
+          />
+          <Text style={styles.text}>{song.title}</Text>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View style={{ flex: 1, height: 1, backgroundColor: "grey" }} />
+            <View>
+              <Text
+                onPress={() => Linking.openURL(`${song.url}`)}
+                style={{
+                  textAlign: "center",
+                  paddingHorizontal: 8,
+                  color: "blue",
+                }}
+              >
+                Shazam
+              </Text>
+            </View>
+            <View style={{ flex: 1, height: 1, backgroundColor: "grey" }} />
+          </View>
+          <Image
+            style={styles.artistart}
+            source={{ uri: `${song.images.background}` }}
+          />
+          <Text style={styles.text}>{song.subtitle}</Text>
         </View>
-      </TouchableOpacity>
+      ) : null}
     </View>
   );
 }
-
--- src/components/styles.jsx
+++ src/components/styles.jsx
@@ -2,23 +2,49 @@ import { StyleSheet } from "react-native";
 export default styles = StyleSheet.create({
   container: {
     height: "100%",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#9ED8DB",
   },
   button: {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
-    backgroundColor: 'blue', // Change the color as needed
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10,
+    borderWidth:2,
+    borderRadius: 25,
+    backgroundColor: "white",
   },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
+  inputContainer: {
+    margin: 15
   },
+  input: {
+    height: 40,
+    width:"100%",
+    backgroundColor: "white",
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth:2,
+    margin: 10,
+  },
+  songContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth:2,
+    margin: 25,
+    borderRadius:25,
+    backgroundColor: "white",
+  },
+  coverart: {
+    width: 100,
+    height: 100,
+    margin: 15,
+  },
+  artistart: {
+    width: 100,
+    height: 100,
+    margin: 15,
+    borderRadius: 100
+  },
+  text: {
+    fontSize:16,
+    margin:10
+  }
 });
